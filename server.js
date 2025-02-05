2f305c0c4bf3724de1b37b11b952d08409d6254e
Xiantu <j6458221@yahoo.com.tw>
2025-02-06 01:48:23 +0800

    add url、method

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const http = require('http');
 
 const requestListener = (req, res)=>{
+  console.log(req.url);
+  console.log(req.method);
+  
   res.writeHead(200,{"Content-Type":"text/plain"});
   res.write('hi');
   res.end();
