9cc01e7055ec31d389e23b2063f763c1eca5005a
Xiantu <j6458221@yahoo.com.tw>
2025-02-07 00:48:07 +0800

    調整表頭資訊，設置回傳JSON與CORS資訊

--- server.js
+++ server.js
@@ -2,13 +2,22 @@ const http = require('http');
 
 const requestListener = (req, res) => {
   const headers = {
-    'Content-Type': 'text/plain',
+    'Access-Control-Allow-Headers':
+      'Content-Type, Authorization, Content-Length, X-Requested-With',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+    'Content-Type': 'application/json',
   };
   console.log(req.url);
   console.log(req.method);
   if (req.url == '/' && req.method == 'GET') {
     res.writeHead(200, headers);
-    res.write('index');
+    res.write(
+      JSON.stringify({
+        status: 'success',
+        data: [],
+      })
+    );
     res.end();
   } else if (req.url == '/' && req.method == 'DELETE') {
     res.writeHead(200, headers);
@@ -16,7 +25,12 @@ const requestListener = (req, res) => {
     res.end();
   } else {
     res.writeHead(404, headers);
-    res.write('not found 404');
+    res.write(
+      JSON.stringify({
+        status: 'false',
+        message: '無此網站路由',
+      })
+    );
     res.end();
   }
 };
