10026a86cc40309fe3f1b995c0a7ab6ad8b77908
Xiantu <j6458221@yahoo.com.tw>
2025-02-07 00:35:51 +0800

    新增首頁測試網址與404

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const http = require('http');
 
-const requestListener = (req, res)=>{
+const requestListener = (req, res) => {
+  const headers = {
+    'Content-Type': 'text/plain',
+  };
   console.log(req.url);
   console.log(req.method);
-  
-  res.writeHead(200,{"Content-Type":"text/plain"});
-  res.write('hi');
-  res.end();
-}
+  if (req.url == '/' && req.method == 'GET') {
+    res.writeHead(200, headers);
+    res.write('index');
+    res.end();
+  } else if (req.url == '/' && req.method == 'DELETE') {
+    res.writeHead(200, headers);
+    res.write('delete');
+    res.end();
+  } else {
+    res.writeHead(404, headers);
+    res.write('not found 404');
+    res.end();
+  }
+};
 
 const server = http.createServer(requestListener);
 server.listen(3005);
